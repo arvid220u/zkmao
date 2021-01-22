e21f4b8ad47a79d1fe446e3b27b65ac2d6808c44
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-01-22 16:18:40 -0500

    add libify!!!

---
+++ circuits/libify.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+declare -a arr=("exponentiate" "ithk" "modulo" "random" "drawcardspublicly")
+
+for i in "${arr[@]}"
+do
+   cp $i/circuit.circom $i/libcircuit.circom
+   sed -i '' -e '$ d' $i/libcircuit.circom
+done
