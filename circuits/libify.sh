e0749bde1ae12d9fa7a9c0ba5db7cb44974db89b
Mark Jabbour <markmarjabbour@gmail.com>
2021-01-26 15:31:07 -0500

    fixed libify

--- circuits/libify.sh
+++ circuits/libify.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-declare -a arr=("exponentiate" "ithk" "modulo" "random" "drawcardspublicly")
+declare -a arr=("exponentiate" "ithk" "modulo" "random" "drawcardspublicly" "accessBit")
 
 for i in "${arr[@]}"
 do
    cp $i/circuit.circom $i/libcircuit.circom
-   sed -i '' -e '$ d' $i/libcircuit.circom
+   sed -i -e '$ d' $i/libcircuit.circom
 done
