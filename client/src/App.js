6c124c2c4b5f26fcd5581aabed504c7dff1244bd
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-01-26 11:44:27 -0500

    add prettier!!!

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,80 @@
-import './App.css';
-import React from 'react';
-import { useRef, useEffect, useState } from 'react';
+import "./App.css";
+import React from "react";
+import { useRef, useEffect, useState } from "react";
 import * as p2p from "./p2p.js";
+
+import { Game } from "./Game.js";
 
 function Create1(props) {
   return (
     <div>
-      <button onClick={() => p2p.createOffer(props.connRef.current, props.setMyOffer)}>Create game</button>
+      <button
+        onClick={() => p2p.createOffer(props.connRef.current, props.setMyOffer)}
+      >
+        Create game
+      </button>
     </div>
-  )
+  );
 }
 
 function Create2(props) {
   const [joinKey, setJoinKey] = useState("");
   return (
     <div>
-      send this message to your friends: <code>{props.offer}</code><br/>
+      send this message to your friends: <code>{props.offer}</code>
+      <br />
       input their answer:
-      <input type="text" value={joinKey} onChange={(e) => setJoinKey(e.target.value)}></input>
-      <button onClick={() => p2p.acceptAnswer(props.connRef.current, joinKey)}>Add player</button>
+      <input
+        type="text"
+        value={joinKey}
+        onChange={(e) => setJoinKey(e.target.value)}
+      ></input>
+      <button onClick={() => p2p.acceptAnswer(props.connRef.current, joinKey)}>
+        Add player
+      </button>
     </div>
-  )
+  );
 }
 
 function Join1(props) {
   const [joinKey, setJoinKey] = useState("");
   return (
     <div>
-      <input type="text" value={joinKey} onChange={(e) => setJoinKey(e.target.value)}></input>
-      <button onClick={() => p2p.join(props.connRef.current, joinKey, props.setMyAnswer)}>Join</button>
+      <input
+        type="text"
+        value={joinKey}
+        onChange={(e) => setJoinKey(e.target.value)}
+      ></input>
+      <button
+        onClick={() =>
+          p2p.join(props.connRef.current, joinKey, props.setMyAnswer)
+        }
+      >
+        Join
+      </button>
     </div>
-  )
+  );
 }
 
 function Join2(props) {
   return (
     <div>
-      send this message to your friends: <code>{props.answer}</code><br/>
+      send this message to your friends: <code>{props.answer}</code>
+      <br />
     </div>
-  )
+  );
 }
 
 function Welcome(props) {
   console.log(props.connRef);
   return (
     <div>
-      <Create1 connRef={props.connRef} setMyOffer={props.setMyOffer}/><br/>
-      <Join1 connRef={props.connRef} setMyAnswer={props.setMyAnswer}/><br/>
+      <Create1 connRef={props.connRef} setMyOffer={props.setMyOffer} />
+      <br />
+      <Join1 connRef={props.connRef} setMyAnswer={props.setMyAnswer} />
+      <br />
     </div>
-  )
+  );
 }
 
 function Lobby(props) {
@@ -62,28 +88,31 @@ function Lobby(props) {
   }
 
   function newMessage(m) {
-    setMessages(oldm => oldm + "\n" + m);
+    setMessages((oldm) => oldm + "\n" + m);
   }
 
   useEffect(() => {
     props.connRef.current.onMessage = (m) => newMessage(m.message);
     return () => {
       props.connRef.current.onMessage = null;
-    }
+    };
   }, [props.connRef]);
 
   return (
     <div>
-      {props.offer ? <Create2 connRef={props.connRef} offer={props.offer}/> : <Join2 connRef={props.connRef} answer={props.answer}/>}
-      <hr/>
-      Participants list: idk<br/>
-      <hr/>
-      <input type="text" value={chatMessage} onChange={(e) => setChatMessage(e.target.value)}></input>
-      <button onClick={onSendMessage}>Send message</button>
-      messages:
-      <p style={{whiteSpace: "pre-line"}}>{messages}</p>
+      {props.offer ? (
+        <Create2 connRef={props.connRef} offer={props.offer} />
+      ) : (
+        <Join2 connRef={props.connRef} answer={props.answer} />
+      )}
+      <hr />
+      Participants list: idk
+      <br />
+      <hr />
+      <button onClick={props.startGame}>Start game!</button>
+      <hr />
     </div>
-  )
+  );
 }
 
 function Setup(props) {
@@ -91,15 +120,25 @@ function Setup(props) {
   const [myAnswer, setMyAnswer] = useState(null);
 
   if (myOffer || myAnswer) {
-    return <Lobby connRef={props.connRef} offer={myOffer} answer={myAnswer}/>;
+    return (
+      <Lobby
+        connRef={props.connRef}
+        offer={myOffer}
+        answer={myAnswer}
+        startGame={props.startGame}
+      />
+    );
   } else {
     return (
       <React.Fragment>
-        <Welcome connRef={props.connRef} setMyOffer={setMyOffer} setMyAnswer={setMyAnswer}/>
+        <Welcome
+          connRef={props.connRef}
+          setMyOffer={setMyOffer}
+          setMyAnswer={setMyAnswer}
+        />
       </React.Fragment>
-    )
+    );
   }
-
 }
 
 function App() {
@@ -112,8 +151,10 @@ function App() {
 
   return (
     <div className="App">
-      {inSetup && <Setup connRef={connRef}/>}
-      {!inSetup && "yay"}
+      {inSetup && (
+        <Setup connRef={connRef} startGame={() => setInSetup(false)} />
+      )}
+      {!inSetup && <Game connRef={connRef} />}
     </div>
   );
 }
