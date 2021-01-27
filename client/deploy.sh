3703f391ea5a75b71cbd9b2f85f28b491bcb2812
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-01-26 22:37:13 -0500

    add deploy script

---
+++ client/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function checkout_web {
+    git checkout web
+}
+
+npm run build
+if ! checkout_web; then
+    echo "error!"
+    exit -1
+fi
+cd build && cp -r * ../ && cd ..
+git add .
+git c -m 'publish site :)'
+git push
+git checkout main
