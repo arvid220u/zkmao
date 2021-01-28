a5ac859321e7f0a17cda39c0f3b08dcead8fb4c7
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-01-27 23:12:25 -0500

    make deploy script better???

--- client/deploy.sh
+++ client/deploy.sh
@@ -4,13 +4,17 @@ function checkout_web {
     git checkout web
 }
 
+GIT_HASH=$(git rev-parse --short HEAD)
+
 npm run build
 if ! checkout_web; then
     echo "error!"
     exit -1
 fi
 cd build && cp -r * ../ && cd ..
+echo $GIT_HASH > version.txt
 git add .
-git c -m 'publish site :)'
+git c -m "publish site :) $GIT_HASH"
 git push
 git checkout main
+echo "version: $GIT_HASH"
